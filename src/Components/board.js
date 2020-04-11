1788ee59515f9cf7d35cd0c8cc947493bcc4fad7
Yuliaperegudova <yuse4ka08@gmail.com>
2020-04-11 12:45:56 +0300

    разбила модули на компоненты, подключила webpack

---
+++ src/Components/board.js
@@ -0,0 +1,7 @@
+export const createBoardTemplate = () => { // доска для карточек
+  return (
+    `<section class="board container">
+      <div class="board__tasks"></div>
+    </section>`
+  );
+};
